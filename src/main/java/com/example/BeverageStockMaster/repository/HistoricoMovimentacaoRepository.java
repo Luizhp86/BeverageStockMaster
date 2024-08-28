38820d337825988e45d821d19838f5b3750ec755
pinotti <luiz.pinotti@viavarejo.com.br>
2024-08-27 21:39:50 -0300

    exclusão seção ok

--- src/main/java/com/example/BeverageStockMaster/repository/HistoricoMovimentacaoRepository.java
+++ src/main/java/com/example/BeverageStockMaster/repository/HistoricoMovimentacaoRepository.java
@@ -1,8 +1,5 @@
 package com.example.BeverageStockMaster.repository;
-
 import com.example.BeverageStockMaster.domain.HistoricoMovimentacao;
-import com.example.BeverageStockMaster.domain.Secao;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
